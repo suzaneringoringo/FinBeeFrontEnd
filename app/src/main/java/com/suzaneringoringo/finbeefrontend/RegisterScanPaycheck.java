a29817733e2028a6c80291f551d875a92667d505
Suzane Ringoringo <suzaneringoringo@gmail.com>
2018-03-20 07:33:08 +0700

    Edit scan view

--- app/src/main/java/com/suzaneringoringo/finbeefrontend/RegisterScanPaycheck.java
+++ app/src/main/java/com/suzaneringoringo/finbeefrontend/RegisterScanPaycheck.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 /**
  * Created by root on 28/02/18.
@@ -17,7 +18,7 @@ class RegisterScanPaycheck extends AppCompatActivity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_scan_paycheck);
-        LinearLayout ll = (LinearLayout) findViewById(R.id.sign_up_7_linear_layout);
+        RelativeLayout ll = (RelativeLayout) findViewById(R.id.sign_up_7_linear_layout);
         ll.setBackgroundColor(Color.argb(0xff, 0xff, 0xff, 0xff));
         overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
     }
--- app/src/main/java/com/suzaneringoringo/finbeefrontend/RegisterScanPhoto.java
+++ app/src/main/java/com/suzaneringoringo/finbeefrontend/RegisterScanPhoto.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 /**
  * Created by root on 27/02/18.
@@ -17,7 +18,7 @@ class RegisterScanPhoto extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_scan_photo);
-        LinearLayout ll = (LinearLayout) findViewById(R.id.sign_up_3_linear_layout);
+        RelativeLayout ll = (RelativeLayout) findViewById(R.id.sign_up_3_linear_layout);
         ll.setBackgroundColor(Color.argb(0xff, 0xff, 0xff, 0xff));
         overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
     }
